0186838be77edb93218501e9c4240c1383368b92
snrntpa2z@naver.com <snrntpa2z@naver.com>
2019-04-02 16:54:35 +0900

    create-react-app 셋팅

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Skills
+
+- Node.js
+- Sass
+- React
+- Mysql
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Hello</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
